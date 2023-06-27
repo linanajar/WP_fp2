8b36d755d1f1f1fd5fae5a630e2a529426d09603
BenBlankenborg <b.b.g.blankenborg@student.rug.nl>
2023-06-27 13:26:18 +0200

    Ensure player boards are generated and variables arent defined twice

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     let rollButton = $("#rollButton");
-    let diceResult = $("#diceResult");
-    let diceValue;
+    //let diceResult = $("#diceResult");
+    //let diceValue;
 
     function rollDice() {
         diceValue = Math.floor(Math.random() * 11) + 2;
@@ -14,18 +14,22 @@ $(document).ready(function() {
             method: "POST",
             data: {diceValue: diceValue.toString()},
             dataType: "text",
-            success: function() {
+            success: function () {
                 console.log("gelukt");
             },
-            error: function(error) {
+            error: function (error) {
                 console.error("nee", error);
             }
         });
     }
 
     rollButton.on("click", rollDice);
-});
+    var player1Board = $("#player1");
+    var player2Board = $("#player2");
 
+    // Arrays to hold the player tiles
+    var player1Tiles = [];
+    var player2Tiles = [];
 
     // Generates player boards
     function generatePlayerBoard(board, playerName, playerTiles) {
@@ -43,7 +47,6 @@ $(document).ready(function() {
     generatePlayerBoard(player1Board, "Player 1", player1Tiles);
     generatePlayerBoard(player2Board, "Player 2", player2Tiles);
 
-    var rollButton = $("#rollButton");
     var diceResult = $("#diceResult");
     var messageText = $("#messageText");
 
@@ -52,7 +55,7 @@ $(document).ready(function() {
     var tileButtons = $(".player-board button");
 
     // Add event listeners to tile buttons
-    tileButtons.on("click", function() {
+    tileButtons.on("click", function () {
         var player = $(this).attr("data-player");
         var tile = parseInt($(this).attr("data-tile"));
 
@@ -63,25 +66,25 @@ $(document).ready(function() {
     });
 
     var diceValue;
-    rollButton.on("click", function() {
+    rollButton.on("click", function () {
         diceValue = Math.floor(Math.random() * 11) + 2;
         diceResult.text("Dice result: " + diceValue);
     });
 
     var submitButton = $("#submit-choice");
-    submitButton.on("click", function() {
+    submitButton.on("click", function () {
         var currentPlayerTiles = currentPlayer === "Player 1" ? player1Tiles : player2Tiles;
 
         // Find selected tiles
         var selectedTiles = [];
-        tileButtons.each(function() {
+        tileButtons.each(function () {
             var player = $(this).attr("data-player");
             var tile = parseInt($(this).attr("data-tile"));
 
             if (player === currentPlayer && tile !== -1 && $(this).hasClass("selected")) {
                 selectedTiles.push(tile);
                 $(this).removeClass("selected");
-                console.log(selectedTiles);
+                //console.log(selectedTiles);
             }
         });
 
@@ -94,28 +97,28 @@ $(document).ready(function() {
             url: "scripts/ajax.handler.php",
             method: "POST",
             data: requestData,
-            success: function(response) {
+            success: function (response) {
                 // Handle the response from the server
-                console.log(requestData);
+                // console.log(requestData);
                 // ...
             },
-            error: function(error) {
+            error: function (error) {
                 console.error("Error updating game state:", error);
                 // ...
             }
         });
 
         // Check if selected tiles sum up to dice value
-        var sum = selectedTiles.reduce(function(acc, curr) {
+        var sum = selectedTiles.reduce(function (acc, curr) {
             return acc + curr;
         }, 0);
 
         if (sum === diceValue) {
             // Close selected tiles
-            selectedTiles.forEach(function(tile) {
+            selectedTiles.forEach(function (tile) {
                 var index = currentPlayerTiles.indexOf(tile);
                 currentPlayerTiles[index] = -1; // Mark the tile as closed
-                tileButtons.each(function() {
+                tileButtons.each(function () {
                     var player = $(this).attr("data-player");
                     var buttonTile = parseInt($(this).attr("data-tile"));
 
@@ -127,28 +130,37 @@ $(document).ready(function() {
             });
         }
 
-        // Check if player has closed all tiles
-        if (currentPlayerTiles.every(function(tile) {
-            return tile === -1;
-        })) {
-            // Display a message and determine the winner
-            // messageText.text(currentPlayer + " wins!");
-            // rollButton.prop("disabled", true);
+        // Check if sum of open tiles is more than dice value
+        var openTiles = ['hey',];
+        tileButtons.each(function () {
+            var player = $(this).attr("data-player");
+            if (player === currentPlayer && !($(this).hasClass("closed"))) {
+                openTiles.push(tile);
+                console.log(openTiles);
+            }
+            if (currentPlayerTiles.every(function (tile) {
+                return tile === -1;
+            })) {
+                // Display a message and determine the winner
+                // messageText.text(currentPlayer + " wins!");
+                // rollButton.prop("disabled", true);
 
-            // Determine the winner
-            var winnerUsername = currentPlayer;
+                // Determine the winner
+                var winnerUsername = currentPlayer;
 
-            // Save the winnerUsername in localStorage
-            localStorage.setItem('winnerUsername', winnerUsername);
+                // Save the winnerUsername in localStorage
+                localStorage.setItem('winnerUsername', winnerUsername);
 
-            // Redirect to endgame.php
-            window.location.href = "http://localhost:8888/WP23/WP_fp/endpage.php";
-            return;
-        }
+                // Redirect to endgame.php
+                window.location.href = "http://localhost:8888/WP23/WP_fp/endpage.php";
+                return;
+            }
 
-        // Switch to next player
-        currentPlayer = currentPlayer === "Player 1" ? "Player 2" : "Player 1";
-        messageText.text(currentPlayer + "'s turn. Select tiles and roll again.");
+            // Switch to next player
+            currentPlayer = currentPlayer === "Player 1" ? "Player 2" : "Player 1";
+            messageText.text(currentPlayer + "'s turn. Select tiles and roll again.");
+        });
     });
+});
 
 
