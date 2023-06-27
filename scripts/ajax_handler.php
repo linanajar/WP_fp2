5b862041da7a3f9a310c6d7428f2d0bcb7453332
Hinke <h.oevering@student.rug.nl>
2023-06-27 10:50:45 +0200

    Fixed some indentation errors

--- scripts/ajax_handler.php
+++ scripts/ajax_handler.php
@@ -6,9 +6,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 //    $diceValue = $_POST["diceValue"];
 
-    $json_data = json_encode(["diceValue" => $diceValue]);
+        $json_data = json_encode(["diceValue" => $diceValue]);
 
-    file_put_contents('../data/gameState.json', $json_data);
+        file_put_contents('../data/gameState.json', $json_data);
     } else {
         echo 'diceValue is missing';
     }
