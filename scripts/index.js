7c3bcecf0d6aea8e993d7dee52662f338091b93d
BenBlankenborg <b.b.g.blankenborg@student.rug.nl>
2023-06-28 23:15:58 +0200

    adjust variables make things more fitting

--- scripts/index.js
+++ scripts/index.js
@@ -5,35 +5,36 @@ $(document).ready(function() {
     var usernameInput = $("#username");
     var linkInput = $("#linkInput");
 
-
     // Define function to generate link
-    function generateLink(usernameInput) {
+    function generateLink(usernameInput, startGameButton, copyButton) {
         let username = usernameInput.val();
         if (username !== '') {
             let gameURL = window.location.origin + "/WP23/WP_fp2/gamepage.php?username=" + encodeURIComponent(username)
             linkInput.val(gameURL);
+            startGameButton.on("click", function() {
+                startGame(gameURL)
+            });
+            copyButton.on("click", copyURL)
         } else {
 
         }
     }
 
     // Define function to start game
-    function startGame() {
+    function startGame(gameURL) {
         window.open(gameURL, "_blank");
     }
 
     // Define function to copy url
     function copyURL() {
         // Copy the game URL to the clipboard
-        linkInput.select();
+        linkInput.val().select();
         document.execCommand("copy");
         window.alert("Game URL copied to clipboard!");
     }
 
     // If username is provided in the query, make the buttons work
     generateLinkButton.on("click", function () {
-        generateLink(usernameInput)
+        generateLink(usernameInput, startGameButton, copyButton)
     });
-    startGameButton.on("click", startGame);
-    copyButton.on("click", copyURL);
 })
