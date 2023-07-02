9a83f07de3dd5e2398cb99a403a66d64ef95e231
javligten <99410951+javligten@users.noreply.github.com>
2023-07-02 15:41:48 +0200

    Set interval for updating gamestate

--- scripts/game.js
+++ scripts/game.js
@@ -208,6 +208,7 @@ $(document).ready(function() {
         currentPlayer = currentPlayer === "Player 1" ? "Player 2" : "Player 1";
         messageText.text(currentPlayer + "'s turn. Select tiles and roll again.");
         });
+    setInterval(updateGameState,3000);
     });
 
 
