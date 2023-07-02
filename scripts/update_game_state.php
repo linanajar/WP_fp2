a3c5d02cd4bb3a98093e4c98750d2bf7d9a2df92
Hinke <h.oevering@student.rug.nl>
2023-07-02 19:15:27 +0200

    Deleted more old code

--- scripts/update_game_state.php
+++ scripts/update_game_state.php
@@ -29,34 +29,3 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
 } else {
     echo 'nope';
 }
-
-//Old code
-//if ($_SERVER["REQUEST_METHOD"] === "GET") {
-////    if (isset($_GET['diceValue'])) {
-////        $diceValue = $_GET['diceValue'];
-////
-////        $jsonData = file_get_contents('../data/gameState.json');
-////        $gameState = json_decode($jsonData, true);
-////
-////        $gameState['diceValue'] = $diceValue;
-////        $jsonData = json_encode($gameState);
-////        file_put_contents('../data/gameState.json', $jsonData);
-////
-////        // Send the updated game state as the response
-////        header('Content-Type: application/json');
-////        echo json_encode($gameState);
-//    if (isset($_GET['diceValue'])) {
-//        $diceValue = $_GET['diceValue'];
-//        echo "Received diceValue: " . $diceValue . "<br>";
-//        $jsonData = file_get_contents('../data/gameState.json');
-//        $gameState = json_decode($jsonData, true);
-//
-//        // Send the game state as the response
-//        header('Content-Type: application/json');
-//        echo json_encode($gameState);
-//    } else {
-//        echo 'geen dicevalue';
-//    }
-//} else {
-//    echo 'Nope';
-//}
